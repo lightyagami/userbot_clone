5423bf03a2190807418bc22a914d967502c9f1d5
Light Yagami <markrayon5@gmail.com>
2022-04-29 19:38:46 +0530

    Update and rename ultroid.sh to test3.sh

---
+++ test3.sh
@@ -0,0 +1,7 @@
+git clone -b main https://github.com/lightyagami/test3
+cp CREDS/.env /root/tes3/.env
+cd /root/test3
+sudo apt-get install python3-pip ffmpeg
+pip3 install -U pip
+pip3 install -U -r requirements.txt
+python3 main.py
