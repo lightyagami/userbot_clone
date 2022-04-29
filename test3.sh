5bb3af6dcf6dceee5bde42673a10122d5217b0e9
Light Yagami <markrayon5@gmail.com>
2022-04-29 20:08:00 +0530

    Test3

--- test3.sh
+++ test3.sh
@@ -1,7 +1,8 @@
-git clone -b main https://github.com/lightyagami/test3
+git clone https://github.com/lightyagami/test3 /root/test3
 cp CREDS/.env /root/tes3/.env
 cd /root/test3
 sudo apt-get install python3-pip ffmpeg
 pip3 install -U pip
 pip3 install -U -r requirements.txt
 python3 main.py
+
