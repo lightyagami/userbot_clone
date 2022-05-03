a7fcb4036319b09893af56406590cc7d253ef349
Light Yagami <markrayon5@gmail.com>
2022-05-03 21:59:04 +0530

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -1,8 +1,15 @@
-git clone https://github.com/lightyagami/test3 /root/test3
-cp CREDS/.env /root/tes3/.env
+git clone https://github.com/lightyagami/py-music-bot /root/test3
+cp CREDS/config.toml /root/tes3/config.env
 cd /root/test3
+
 sudo apt-get install python3-pip ffmpeg
-pip3 install -U pip
-pip3 install -U -r requirements.txt
-python3 main.py
+sudo apt-get update && apt-get install -y ffmpeg
+pip3 install opuslib
+pip3 install opus-api
+pip3 install pipenv
+pipenv install
+pipenv shell
+
+#pip3 install -U -r requirements.txt
+python3 -m musicbot
 
