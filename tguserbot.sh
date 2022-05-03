dba72cbce2607d14be31793e6d47a0ac1005b189
Light Yagami <markrayon5@gmail.com>
2022-05-03 22:46:26 +0530

    Update

--- tguserbot.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/kandnub/TG-UserBot/ /root/tguserbot
-cp CREDS/config.ini /root/tguserbot/config.ini
-cd /root/tguserbot
-pip3 install -r requirements.txt
-python3 -m userbot
